c4dc07a4b4d7d0dbae8bf9872704b572b0d5c95c
sashamikhalin2017 <sashamikhalin2017@yandex.ru>
2017-11-19 03:12:39 +0300

    add post models

---
+++ Charity/Charity/Models/PostModels/AdvPostModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Charity.Models.PostModels
+{
+    public class AdvPostModel { 
+       public string Name { get; set; }
+       public string Description { get; set; }
+       public ItemPostModel items { get; set; }
+    }
+}
---
+++ Charity/Charity/Models/PostModels/ItemPostModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Charity.Models.PostModels
+{
+    public class ItemPostModel
+    {
+       public string Name { get; set; }
+       public string Description { get; set; }
+       public ImgPostModel Images { get; set; }
+    }
+}

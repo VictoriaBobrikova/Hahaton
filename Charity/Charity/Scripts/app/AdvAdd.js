7f50cd67080be6b9cfac9ce7368c2180f86d096f
sashamikhalin2017 <sashamikhalin2017@yandex.ru>
2017-11-19 10:55:34 +0300

    added js

---
+++ Charity/Charity/Scripts/app/AdvAdd.js
@@ -0,0 +1,19 @@
+﻿function addItem() {
+    $('.item_container').append($('<div class="col-md-12 adv2 item"><div class="row"><div class="col-md-4"><img src="~/Content/Images/blagotvor.jpg" class="img-circle" style="width:150px;"><br><br><p><input type="file" name="pic" class="form-control" style="width: 145px;"></p></div><div class="col-md-8"><form role="form"><div class="form-group"><label>Название: </label><input type="text" name="name" class="form-control" /><br><label>Информация: </label><textarea rows="3" cols="45" name="descrtiption" class="form-control"></textarea></div></form></div></div></div>'));
+};
+
+function takeAdvById(id) {
+    window.location.href = "/Home/TakeById?id=" + id;
+};
+
+function saveAdv() {
+    var name = document.getElementById("search_input").value;
+    var description = document.getElementById("search_input").value;
+
+    var items = document.getElementsByClassName('item');
+    for (var i = 0; i < items.length; i++) {
+        var name = items[i].getElementsByClassName('name').value;
+        var description = items[i].getElementsByClassName('description').value;
+        var photo = items[i].getElementsByClassName('photo').value;
+    }
+};
